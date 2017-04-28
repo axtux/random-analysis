27783efbef4a7f76fc57060b2b316e4334bb4384
Axtux <axtux@hotmail.com>
2017-04-28 16:33:01 +0200

    add pi digits accessor

---
+++ src/pi.py
@@ -0,0 +1,23 @@
+import file
+
+pi_file = '../data/pi_1.000.000.txt'
+
+def get_digits() :
+  pi = file.get_contents(pi_file)
+  if pi == None :
+    print('Error reading file '+pi_file)
+    return None
+  
+  pi = pi.replace('\r', '').replace('\n', '')
+  
+  # remove "3."
+  return pi[2:]
+
+if __name__ == "__main__" :
+  digits = get_digits()
+  if digits == None :
+    exit()
+  
+  print('Pi 10 first digits are', digits[:10])
+  #l = len(digits)
+  print('Pi 999.990-1.000.000 digits are', digits[-10:])
