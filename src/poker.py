8f873841122ca63d786e3363bf1b0bca770b030e
Axtux <axtux@hotmail.com>
2017-05-02 22:10:42 +0200

    poker test

---
+++ src/poker.py
@@ -0,0 +1,61 @@
+
+"""
+count the number of different digits in each seq_len-digits sequence
+"""
+def count(digits, seq_len = 10) :
+  count = {}
+  for i in range(1, seq_len+1) :
+    count[i] = 0
+  
+  limit = len(digits)-seq_len+1
+  for i in range(0, limit, seq_len) :
+    sub = digits[i:i+seq_len]
+    diff = count_different(sub)
+    count[diff] = count[diff] + 1
+  
+  return count
+
+def count_different(sequence) :
+  return len(set(sequence))
+
+"""
+number of manner to make r groups with k choices
+"""
+def stirling(k, r) :
+  if k < 1 or r < 1 or r > k :
+    return None
+  
+  if r == 1 or r == k :
+    return 1
+  
+  return stirling(k-1, r-1) + r*stirling(k-1, r)
+
+"""
+probability to get r different elements within l-length sequence with k choices
+"""
+def probability(k, r, l) :
+  p = stirling(l, r)
+  for i in range(k-r+1, k+1) :
+    p = p * i
+  return p/k**l
+
+def expected_probability(observed) :
+  seq_len = len(observed)
+  
+  total = 0
+  for k in observed :
+    total = total + observed[k]
+  
+  expected = {}
+  for k in observed :
+    p = probability(10, k, seq_len)
+    expected[k] = round(total*p)
+  return expected
+
+if __name__ == "__main__" :
+  import pi
+  digits = pi.get_digits()
+  observed = count(digits)
+  expected = expected_probability(observed)
+  import test
+  test.make_test(observed, expected)
