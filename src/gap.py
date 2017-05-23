b08c2689918da07845cac7743d34aa91c6be8c2a
Marco Salemi <vondutchstyleas@hotmail.com>
2017-05-23 04:14:47 +0200

    modification proba

--- src/gap.py
+++ src/gap.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 gaps={}
 
 def mark(digits,a,b):
@@ -14,35 +12,45 @@ def mark(digits,a,b):
             #print (value,False)
     return rep
 
-def realGaps(marked,r):
+def realGaps(digits,r,a,b):
+
+    marked = mark(digits,a,b)
     rep = {}
-    for i in range(r):#initialise the list
+
+    for i in range(r+1):#initialise the list
         rep[i]=0
+
     inGap = False
     gap = 0
     for i in range(len(marked)):
         if (not marked[i]):
             gap +=1
             inGap=True
-        if (marked[i] and inGap):
+        if (marked[i]):
             rep[gap]+=1
             gap=0
             inGap=False
     return rep
+    #on teste les gaps qui vont jusque 35
 
 def expected_gaps(digits,r,a,b):
     rep={}
-    p=abs(a-b)
+    p=(b-a)+0.1
     n=len(digits)
     for i in range(r+1):
         rep[i]= p*((1-p)**i)
-        rep[i]*=n
+        rep[i]*=n*p
     return rep
 
 if __name__ == "__main__" :
   import pi
   digits = pi.get_digits()
-  observed = realGaps(mark(digits,0,0.5),35)
-  expected = expected_gaps(digits,35,0,0.5)
+
+  """test = realGaps([1,7,8,9,7,8],6,0,0.5)
+  for e in test.items():
+      print( e[0]," -- ",e[1])"""
+
+  observed = realGaps(digits,25,0,0.5)
+  expected = expected_gaps(digits,25,0,0.5)
   import test
   test.make_test(observed, expected, 'Gap', 'Tailles de gap', 'Nombre d\'occurences')
