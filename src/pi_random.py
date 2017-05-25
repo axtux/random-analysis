eb6051850d5106e5146733a41412940bafba21bb
Marco Salemi <vondutchstyleas@hotmail.com>
2017-05-25 13:06:45 +0200

    modification path to 13

--- src/pi_random.py
+++ src/pi_random.py
@@ -10,28 +10,28 @@ def init():
   - add randomness to start point with ms time
   """
   global digits, l, i
-  
+
   # get pi digits and calculate len
   digits = pi.get_digits()
   if digits == None :
     exit('Need digits from pi module')
   l = len(digits)
-  
-  # set default path to 15
-  set_path(15)
-  
+
+  # set default path to 13
+  set_path(13)
+
   # add randomness to start point with ms time
   i = int(time.time()*1000) % l
 
 def set_path(p):
   """number of digits to use to make random number"""
   global path, mult, digits
-  
+
   if not is_int(p):
     return print('p must be an integer')
   if p > l:
     return print('p max value is {}'.format(l))
-  
+
   path = p
   # multiplication to get float in [0,1[
   mult = 10**(-path)
@@ -40,15 +40,22 @@ def set_path(p):
 
 def random() :
   global i
-  
+
   i = (i+path)%l
-  
+
   s = digits[i:i+path]
-  
+
   return int(s) * mult
 
+def generate(number):
+    init()
+    rep=[]
+    for i in range(number):
+        rep.append(random())
+    return rep
 
 init()
 if __name__ == "__main__" :
-  for k in range(0, 5) :
-    print(random())
+  num = generate(20)
+  for i in num :
+      print (i)
